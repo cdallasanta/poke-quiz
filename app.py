29ca9324100ba9aecee29674866b62ac6fbb0c12
cdallasanta <chris.dallasanta@gmail.com>
2020-02-02 23:31:30 -0800

    added prompt for difficulty

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-from pokemon import Pokemon
 from quiz import Quiz
 
 def main():
-  quiz = Quiz()
+  print("Welcome trainer!")
+  print("Please select a difficulty:")
+  print("0: Easy - The pokemon's types are shown, only need to guess one correct answer")
+  print("1: Medium - The pokemon's types are shown,  must guess all correct answers")
+  print("2: Hard - The pokemon's types are not shown, only need to guess one correct answer")
+  print("3: Very Difficult - The pokemon's types are not shown, must guess all correct answers")
+  difficulty = input()
+  quiz = Quiz(difficulty)
   quiz.run()
 
 if __name__ == "__main__":
--- quiz.py
+++ quiz.py
@@ -5,23 +5,17 @@ import pdb
 class Quiz:
   scraped_pokemon = []
   
-  def __init__(self):
+  def __init__(self, difficulty):
+    self.difficulty = difficulty
     self.scraper = Scraper()
     self.national_dex = self.scraper.set_national_dex()
   
   def run(self):
     poke_id = random.randint(0,len(self.national_dex)-1)
-    # if it's in scraped_pokemon, serve pokemon to quiz
     answer_pokemon = next((p for p in self.scraped_pokemon if p["id"] == poke_id+1), None)
-    if answer_pokemon:
-      pdb.set_trace
-      pass
-      # move on
-    else:
+    if not answer_pokemon:
       answer_pokemon = self.scraper.fetch_pokemon_data(self.national_dex[poke_id-1])
       self.scraped_pokemon.append(answer_pokemon)
-    # else, send to fetch_pokemon_data(id)
 
-    pdb.set_trace()
     # quiz part here: ______
     pass
